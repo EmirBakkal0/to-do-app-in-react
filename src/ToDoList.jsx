2af568f7b897fcf966f38ec88a9a95ef650a7375
Emir Bakkal <ebakkal2@gmail.com>
2024-07-04 10:37:10 +0300

    fixed small issue with useState default value

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,82 +1,87 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function ToDoList() {
+  const [todoList, setTodoList] = useState(
+    JSON.parse(window.localStorage.getItem("todos")) ?? []
+  );
+  const [todo, setTodo] = useState("");
 
-    const [todoList,setTodoList] =useState(JSON.parse(window.localStorage.getItem("todos"))
-)
-    const [todo,setTodo] = useState("")
+  console.log(todoList);
 
-    useEffect(() =>{
+  useEffect(() => {
+    window.localStorage.setItem("todos", JSON.stringify(todoList));
+  }, [todoList]);
 
-        window.localStorage.setItem("todos",JSON.stringify(todoList))
+  function handleTodo(event) {
+    setTodo(event.target.value);
+  }
 
-    },[todoList])
+  function handleTodoList() {
+    setTodoList([...todoList, todo]);
+    setTodo("");
+  }
 
+  function handleDelete(index) {
+    setTodoList(todoList.filter((_, i) => i !== index));
+  }
 
-    function handleTodo(event){
+  function moveUp(index) {
+    if (index > 0) {
+      const updatedTodoList = [...todoList];
+      let temp = updatedTodoList[index];
+      updatedTodoList[index] = updatedTodoList[index - 1];
+      updatedTodoList[index - 1] = temp;
 
-        setTodo(event.target.value)
+      setTodoList(updatedTodoList);
     }
+  }
 
-    function handleTodoList(){
-        
-        setTodoList([...todoList,todo])
-        setTodo("")
+  function moveDown(index) {
+    if (index < todoList.length - 1) {
+      const updatedTodoList = [...todoList];
+      let temp = updatedTodoList[index];
+      updatedTodoList[index] = updatedTodoList[index + 1];
+      updatedTodoList[index + 1] = temp;
+
+      setTodoList(updatedTodoList);
     }
+  }
 
-    function handleDelete(index){
-        setTodoList(todoList.filter((_,i) => i!==index))
-    }
-
-    function moveUp(index){
-        if(index>0){
-            const updatedTodoList= [...todoList]
-            let temp = updatedTodoList[index]
-            updatedTodoList[index]=updatedTodoList[index-1]
-            updatedTodoList[index-1]=temp
-
-            setTodoList(updatedTodoList)
-
-        }
-    }
-
-    function moveDown(index){
-        if(index< todoList.length-1){
-            const updatedTodoList= [...todoList]
-            let temp = updatedTodoList[index]
-            updatedTodoList[index]=updatedTodoList[index+1]
-            updatedTodoList[index+1]=temp
-
-            setTodoList(updatedTodoList)
-
-        }
-    }
-
-    return( 
-    
+  return (
     <div className="todo-main">
+      <h1>TODO LIST </h1>
+      <div>
+        <input
+          type="text"
+          value={todo}
+          onChange={handleTodo}
+          placeholder="Enter a Task.."
+        />
 
-        <h1>TODO LIST </h1>
-        <div>
-        <input type="text" value={todo} onChange={handleTodo} placeholder="Enter a Task.." />
-        
-        <button className="add-btn" onClick={handleTodoList} > Add item </button>
-        </div>
+        <button className="add-btn" onClick={handleTodoList}>
+          {" "}
+          Add item{" "}
+        </button>
+      </div>
 
-        <ul className="list">
-            {todoList.map((element,index) =>
-            <li className="list-item" key={index}> <span className="text">{element}</span> 
-            <button className="delete-btn" onClick={() => handleDelete(index)}>  Delete </button>
-            <button className="move-btn" onClick={() => moveUp(index)}>Move up</button>
-            <button className="move-btn" onClick={() => moveDown(index)}>Move down</button>
-
-            </li> )}
-
-        </ul>
-        
-
-    </div> 
-    
-     
-)
+      <ul className="list">
+        {todoList.map((element, index) => (
+          <li className="list-item" key={index}>
+            {" "}
+            <span className="text">{element}</span>
+            <button className="delete-btn" onClick={() => handleDelete(index)}>
+              {" "}
+              Delete{" "}
+            </button>
+            <button className="move-btn" onClick={() => moveUp(index)}>
+              Move up
+            </button>
+            <button className="move-btn" onClick={() => moveDown(index)}>
+              Move down
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
